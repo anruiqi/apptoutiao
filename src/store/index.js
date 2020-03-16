b49c29add50a325ccb2d1757a5d19526b9bcb20b
anruiqi <59909691+anruiqi@users.noreply.github.com>
2020-03-16 09:53:08 +0800

    vuex状态管理token

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import * as auth from '@/utils/auth'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: auth.getUser
   },
+  // 修改state
   mutations: {
+    // 修改token
+    upladteUser (state, payload) {
+      state.user = payload.user
+      // 保持vuex和本地储存同步
+      auth.setUser(payload.user)
+    },
+    // 删除token
+    delUser (state) {
+      state.user = {}
+      // 更新缓存数据
+      // 删除本地缓存的token
+      auth.delUser()
+    }
   },
   actions: {
   },
